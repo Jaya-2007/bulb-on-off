d445b7e70e99e55725dd8428855cbc540dc5d55e
K Jayalaxmi <jayalaxmik227@gmail.com>
2025-11-20 13:05:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function onClick() {
+    let bulb = document.getElementById("bulbimage");
+    bulb.src="https://th.bing.com/th/id/OIP.B1GrAuZqjEREj1zZtdSIMwHaH_?w=190&h=205&c=7&r=0&o=7&dpr=1.6&pid=1.7&rm=3";
+}
+
+function offClick() {
+    let bulb = document.getElementById("bulbimage");
+    bulb.src="https://www.bing.com/images/search?view=detailV2&ccid=R46Rew%2fS&id=DAED4AB73C9B4A5AE57471910B2B7B5C91EEC97A&thid=OIP.R46Rew_SubHSNisaARhcnAHaHe&mediaurl=https%3a%2f%2fas2.ftcdn.net%2fv2%2fjpg%2f00%2f77%2f99%2f99%2f1000_F_77999958_tZYT7xWlSJewLgsMYY3CrAWvYITlWYTF.jpg&exph=1000&expw=990&q=bulb+off+image&FORM=IRPRST&ck=4FA68E62D2169B43C1644D0439F82873&selectedIndex=1&itb=0";
+}
